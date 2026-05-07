3f72bd94a16c8de03a5d03524bead92627773d7e
Mahmud Hasan <mahmudhasansadi92@gmail.com>
2026-05-07 12:40:44 +0600

    migration abd model relation update

---
+++ app/Models/Url.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Url extends Model
+{
+    use SoftDeletes;
+
+    protected $table = "urls";
+
+    protected $fillable = [
+        'user_id',
+        'original_url',
+        'short_code',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -34,6 +34,11 @@ class User extends Authenticatable
         'remember_token',
     ];
 
+    public function urls()
+    {
+        return $this->hasMany(Url::class);
+    }
+
     /**
      * Get the attributes that should be cast.
      *
